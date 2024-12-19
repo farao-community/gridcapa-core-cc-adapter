dceb60509fe9441dc6ee7a656d19af69bd102f53
danielthirion <152513244+danielthirion@users.noreply.github.com>
2024-12-19 10:41:40 +0100

    Move from javax to jakarta (#51)
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>

--- src/main/java/com/farao_community/farao/core_cc/adapter/util/JaxbUtil.java
+++ src/main/java/com/farao_community/farao/core_cc/adapter/util/JaxbUtil.java
@@ -6,10 +6,10 @@
  */
 package com.farao_community.farao.core_cc.adapter.util;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import java.io.InputStream;
 
